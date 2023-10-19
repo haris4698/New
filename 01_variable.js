d9cb644d4da75c573e0fe2ad52f94c335274131b
Syed Haris <syedharis46985@gmail.com>
2023-10-19 21:51:58 +0500

    new

---
+++ 01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 145677
+let emailId = "syedhrais@gmail.com"
+var accountpassword = "123455"
+accountCity = "Lahore" 
+let freeState;
+
+
+// accountId = 4455
+
+// updated
+
+emailId = "Haris@gmail.com"
+accountCity = "Multan"
+
+
+
+
+
+console.log(accountpassword);
+console.log(emailId);
+console.table([accountId, emailId, accountpassword,accountCity, freeState]);
---
+++ 02-DataTypes.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Data Types 
+// number => 2 the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+console.log(typeof null); //Object
+console.log(typeof undefined); //undefined
+
+
+
+console.log('Test');
+
+console.log(3+6);
